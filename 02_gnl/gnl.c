8d564e3d03d08f9eec4df579ead51d8c8e4a3ad0
Elodie FINICKEL <efinicke@e2r8p15.clusters.42paris.fr>
2022-01-06 22:49:41 +0100

    gnl_completed

--- 02_gnl/gnl.c
+++ 02_gnl/gnl.c
@@ -1,6 +1,6 @@
 #include "gnl.h"
 
-static int		crush_first_c(char *str, char c1, char c2)
+static int	crush_first_c(char *str, char c1, char c2)
 {
 	int	i;
 
@@ -20,18 +20,20 @@ static int		crush_first_c(char *str, char c1, char c2)
 
 char	*get_next_line(int fd)
 {
-	static char	*bigstring;
+	static char	*bigstring = NULL;
 	char		buf[BUFFER_SIZE + 1];
-	char        *line;
-	int			ret;
+	char		*line;
+	char		*tmp;
 	int			i;
-	char 		*tmp;
+	int			ret;
 
 	ret = 1;
+	tmp = NULL;
+	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	while (((i = crush_first_c(bigstring, '\n', '\0')) == -1) 
-			&& ((ret = read(fd, buf, BUFFER_SIZE)) != 0))
+		return (NULL);
+	while (((i = crush_first_c(bigstring, '\n', '\0')) == -1)
+		&& ((ret = read(fd, buf, BUFFER_SIZE)) != 0))
 	{
 		if (ret == -1)
 			return (NULL);
@@ -39,13 +41,20 @@ char	*get_next_line(int fd)
 		bigstring = ft_strjoin_free(bigstring, buf);
 	}
 	line = ft_strdup(bigstring);
-	tmp = bigstring;
-	bigstring = ft_strdup(&bigstring[i + 1]);
-	free(tmp);
+	if (i != -1)
+	{
+		tmp = bigstring;
+		bigstring = ft_strdup(&bigstring[i + 1]);
+		free(tmp);
+	}
+	if (ret == 0 && bigstring == NULL)
+		return (NULL);
 	if (ret == 0)
 	{
-		free(line);
-		return (NULL);
+		free(bigstring);
+		bigstring = NULL;
+		return (line);
 	}
+	line = ft_strjoin_free(line, "\n");
 	return (line);
 }
